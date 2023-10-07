094f4a50de9df51b221212dcf3ee3cf968c889fe
jakubxy08 <45764280+jakubxy08@users.noreply.github.com>
2023-10-07 22:31:00 +0200

    Add clean_narrative()

--- src/main.py
+++ src/main.py
@@ -1,8 +1,134 @@
 """Analysis of unsupervised wisdom data."""
 
+import json
+import re
+
 import nltk
+import pandas as pd
 import torch
 from tqdm import tqdm
+
+medical_terms = {
+    "&": "and",
+    "***": "",
+    ">>": "clinical diagnosis",
+    "@": "at",
+    "abd": "abdomen",
+    "af": "accidental fall",
+    "afib": "atrial fibrillation",
+    "aki": "acute kidney injury",
+    "am": "morning",
+    "ams": "altered mental status",
+    "bac": "blood alcohol content",
+    "bal": "blood alcohol level,",
+    "biba": "brought in by ambulance",
+    "c/o": "complains of",
+    "chi": "closed-head injury",
+    "clsd": "closed",
+    "cpk": "creatine phosphokinase",
+    "cva": "cerebral vascular accident",
+    "dx": "clinical diagnosis",
+    "ecf": "extended-care facility",
+    "er": "emergency room",
+    "etoh": "ethyl alcohol",
+    "eval": "evaluation",
+    "fd": "fall detected",
+    "fx": "fracture",
+    "fxs": "fractures",
+    "glf": "ground level fall",
+    "h/o": "history of",
+    "htn": "hypertension",
+    "hx": "history of",
+    "inj": "injury",
+    "inr": "international normalized ratio",
+    "intox": "intoxication",
+    "l": "left",
+    "loc": "loss of consciousness",
+    "lt": "left",
+    "mech": "mechanical",
+    "mult": "multiple",
+    "n.h.": "nursing home",
+    "nh": "nursing home",
+    "p/w": "presents with",
+    "pm": "afternoon",
+    "pt": "patient",
+    "pta": "prior to arrival",
+    "pts": "patient's",
+    "px": "physical examination",
+    "r": "right",
+    "r/o": "rules out",
+    "rt": "right",
+    "s'd&f": "slipped and fell",
+    "s/p": "after",
+    "sah": "subarachnoid hemorrhage",
+    "sdh": "acute subdural hematoma",
+    "sts": "sit-to-stand",
+    "t'd&f": "tripped and fell",
+    "tr": "trauma",
+    "uti": "urinary tract infection",
+    "w/": "with",
+    "w/o": "without",
+    "wks": "weeks",
+}
+
+
+def clean_narrative(text: str) -> str:
+    """
+    Preprocess the input text to provide a cleaner narrative.
+
+    Inspired by: https://www.drivendata.org/competitions/217/cdc-fall-narratives/community-code/50/
+    This function performs several text preprocessing steps to transform medical narratives into a more readable format:
+    1. Converts the text to lowercase.
+    2. Modifies mention of 'DX' for readability.
+    3. Removes specific age and sex identifications, replacing them with the general term "patient".
+    4. Translates certain medical terms and symbols into their user-friendly counterparts.
+    5. Formats the text into user-friendly sentences, ensuring proper capitalization and spacing.
+
+    Parameters
+    ----------
+    text : str
+        The raw medical narrative text to be cleaned.
+
+    Returns
+    -------
+    str
+        The preprocessed text in a more user-friendly format.
+
+    """
+    # lowercase everything
+    text = text.lower()
+
+    # unglued DX
+    regex_dx = r"([ˆ\W]*(dx)[ˆ\W]*)"
+    text = re.sub(regex_dx, r". dx: ", text)
+
+    # remove age and sex identifications
+    # regex to capture age and sex (not perfect but captures almost all the cases)
+    regex_age_sex = r"(\d+)\s*?(yof|yf|yo\s*female|yo\s*f|yom|ym|yo\s*male|yo\s*m)"
+    if age_sex_match := re.search(regex_age_sex, text):
+        # age = age_sex_match[1]
+        sex = age_sex_match[2]
+        if "f" in sex or "m" in sex:
+            # text = text.replace(age_sex_match.group(0), f"{age} years old female")
+            text = text.replace(age_sex_match[0], "patient")
+    # translate medical terms
+    for term, replacement in medical_terms.items():
+        if term in ["@", ">>", "&", "***"]:
+            pattern = rf"({re.escape(term)})"
+            text = re.sub(pattern, f" {replacement} ", text)  # force spaces around replacement
+
+        else:
+            pattern = rf"(?<!-)\b({re.escape(term)})\b(?!-)"
+            text = re.sub(pattern, replacement, text)
+
+    # user-friendly format
+    sentences = sent_tokenizer.tokenize(text)
+    sentences = [sent.capitalize() for sent in sentences]
+    sent = " ".join(sentences)
+    for char in ["+", "-", "?"]:
+        sent = sent.replace(char, " ")
+    return sent
+
 
 if __name__ == "__main__":
     # set up
@@ -16,3 +142,11 @@ if __name__ == "__main__":
     device = f"cuda:{torch.cuda.current_device()}" if torch.cuda.is_available() else "cpu"
     cuda_v = torch.version.cuda if device != "cpu" else ""
     f"Running on device: '{device}' with CUDA version '{cuda_v}."
+
+    # load data
+    df_1 = pd.read_csv("data/primary_data.csv")
+    vm_1 = json.load(open("data/variable_mapping.json"))
+
+    # preprocess narrative
+    df_1["text"] = df_1["narrative"].apply(clean_narrative)
+    print(df_1.iloc[1].T)
