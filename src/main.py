1081bd450b9153463aa15969db88132602c1556e
jakubxy08 <45764280+jakubxy08@users.noreply.github.com>
2023-10-07 22:27:01 +0200

    Add set up

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
-s
+"""Analysis of unsupervised wisdom data."""
+
+import nltk
+import torch
+from tqdm import tqdm
+
+if __name__ == "__main__":
+    # set up
+    nltk.download("punkt")
+    nltk.download("stopwords")
+    nltk.download("wordnet")
+    nltk.download("words")
+    sent_tokenizer = nltk.data.load("tokenizers/punkt/english.pickle")
+
+    tqdm.pandas()
+    device = f"cuda:{torch.cuda.current_device()}" if torch.cuda.is_available() else "cpu"
+    cuda_v = torch.version.cuda if device != "cpu" else ""
+    f"Running on device: '{device}' with CUDA version '{cuda_v}."
